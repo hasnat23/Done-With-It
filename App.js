82f17ee14b4eabeaac1ccab3b79ef708627217a6
hasnat23 <chmuhammadhasnat23@gmail.com>
2021-12-19 21:49:47 +0100

    publishing simple testing app

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
   console.log("App executed.")
+
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>

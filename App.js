0782a6273fb53ce4c7348be5a3b8109df921e2f4
hasnat23 <chmuhammadhasnat23@gmail.com>
2021-12-29 23:44:36 +0100

    creating the welcome screen

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import WelcomeScreen from "./app/assets/screens/WelcomeScreen";
 
-export default function App() {
-  console.log("App executed.")
 
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return <WelcomeScreen/>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
---
+++ app/assets/screens/WelcomeScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen(props) {
+  return (
+  <ImageBackground style={styles.background}
+  source={require('../background.jpg')}>
+
+    <View style={styles.logoContainer}>
+    <Image style={styles.logo} source={require('../logo-red.png')}/>
+    <Text>Sell What You Don't need</Text>
+    </View>
+
+
+    <View style={styles.loginButton}></View>
+    <View style={styles.registerButton}></View>
+  </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  loginButton:{
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65'
+  },
+  registerButton:{
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4'
+  },
+  logo:{
+    height: 100,
+    width:100,
+  },
+  logoContainer:{
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center'
+  }
+})
+
+export default WelcomeScreen;

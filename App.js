c14aaf2b20dc96a161c86c1380f301aead490d36
hasnat23 <chmuhammadhasnat23@gmail.com>
2021-12-30 00:13:56 +0100

    creating ViewImageScreen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ViewImageScreen from "./app/assets/screens/ViewImageScreen";
 import WelcomeScreen from "./app/assets/screens/WelcomeScreen";
 
 
 export default function App(){
-  return <WelcomeScreen/>
+  return <ViewImageScreen/>
 }
 
 
